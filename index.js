10394abb030b1f222a1eb43452e254b2b24ffd7e
Stick-z <56492228+Stick-z@users.noreply.github.com>
2021-11-02 10:23:34 -0700

    Test
    Test commit

--- index.js
+++ index.js
@@ -3,22 +3,30 @@ const input = document.getElementById("input");
 var words = [" This", " is", " a", " test", " sentence."];
 var score = 0;
 
-function update() {
-	p.innerHTML = words[score];
-}
+p.innerHTML = words[score];
+p.innerHTML += words[score + 1].fontcolor("green");
 
-update();
 input.value = " "; // Change later to ensure the user doesn't click before the space...
 
 input.addEventListener("keypress", (e) => {
-	if (e.key == " ") {
-		if (input.value == words[score]) {
-			input.value = "";
-			score++;
-			update();
-		} else {
-			input.value = "";
-			p.innerHTML = words[score].fontcolor("red");
-		}
-	}
+  if (e.key == " ") {
+    if (score == words.length) {
+      console.log("yes");
+    } else {
+      if (input.value == words[score]) {
+        input.value = "";
+        score++;
+        p.innerHTML = words[score - 1].fontcolor("green");
+        if (words[score] != undefined) {
+          p.innerHTML += words[score];
+        }
+        if (words[score + 1] != undefined) {
+          p.innerHTML += words[score + 1].fontcolor("green");
+        }
+      } else {
+        input.value = "";
+        p.innerHTML = words[score].fontcolor("red");
+      }
+    }
+  }
 });
