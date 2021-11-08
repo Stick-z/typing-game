a74216e602e07ea769cca662115b6e1d51f6cced
Stick-z <56492228+Stick-z@users.noreply.github.com>
2021-11-08 12:07:14 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,52 @@
 const p = document.getElementById("paragraph");
+const past = document.getElementById("pastWord");
+const word = document.getElementById("word");
+const next = document.getElementById("nextWord");
 const input = document.getElementById("input");
-var words = [" This", " is", " a", " test", " sentence."];
+
+var words = ["This", "is", "a", "test", "sentence."];
 var score = 0;
+var mistakes = 0;
+word.innerHTML = words[0];
+next.innerHTML = words[1];
 
-p.innerHTML = words[score];
-p.innerHTML += words[score + 1].fontcolor("green");
+function checkUndefined(value) {
+	if (value == undefined) {
+		return "";
+	} else {
+		return value;
+	}
+}
 
-input.value = " "; // Change later to ensure the user doesn't click before the space...
+function correctUpdate() {
+	input.value = "";
+	score++;
+	past.innerHTML = words[score - 1].fontcolor("green");
+	word.innerHTML = words[score];
+	next.innerHTML = checkUndefined(words[score + 1]);
+}
+
+function check() {
+	if (input.value.replace(/\s+/g, "") == words[score]) {
+		console.log("Correct.");
+		correctUpdate();
+	} else {
+		console.log("wrong");
+		input.value = "";
+		word.innerHTML = words[score].fontcolor("red");
+	}
+}
 
 input.addEventListener("keypress", (e) => {
-  if (e.key == " ") {
-    if (score == words.length - 1) {
-      console.log("yes");
-    } else {
-      if (input.value == words[score]) {
-        input.value = "";
-        score++;
-        p.innerHTML = words[score - 1].fontcolor("green");
-        if (words[score] != undefined) {
-          p.innerHTML += words[score];
-        }
-        if (words[score + 1] != undefined) {
-          p.innerHTML += words[score + 1].fontcolor("green");
-        }
-      } else {
-        input.value = "";
-        p.innerHTML = words[score].fontcolor("red");
-      }
-    }
-  }
+	if (e.key == " ") {
+		if (score != words.length - 1) {
+			console.log("checking...");
+			check();
+		} else {
+			console.log("complete!");
+			input.value = "";
+			input.readOnly = true;
+			p.innerHTML = "Nice";
+		}
+	}
 });
